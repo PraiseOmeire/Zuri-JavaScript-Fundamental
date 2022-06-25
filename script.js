6f7b0e85fa10e37875573d3e069c43de1e6413b2
Praise Omeire <omeireoluomachi99@gmail.com>
2022-06-25 15:09:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const Name = window.prompt("Praise Omeire is 5ft 6inches and is from Nigeria");
+
+const Myname = " My Name is Praise Omeire";
+const height = " My Height is 5ft 6inches";
+const country = "I'm From Nigeria";
+
+console.log(Myname);
+console.log(height);
+console.log(country);
